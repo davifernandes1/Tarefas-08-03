d9300d31891101d1d11e2cb55c211e2f441b79bd
Davi <99411194+davifernandes1@users.noreply.github.com>
2022-05-10 12:48:01 -0300

    Add files via upload

---
+++ Atividades/Atv de Array/atv1.js
@@ -0,0 +1,6 @@
+nomes = ['Davi','Rafael','Pedro','Bernardo','Anderson']
+
+for(let i = 0; i <nomes.length; i++){
+    console.log(nomes[i])
+}
+
---
+++ Atividades/Atv de Array/atv2.js
@@ -0,0 +1,5 @@
+salarios = ['R$10.000','R$3.000','R$2.500','R$5.750','R$900','R$8.300','R$12.000', 'R$1.590']
+
+for(let i = 0; i <salarios.length; i++){
+    console.log(salarios[i])
+}
